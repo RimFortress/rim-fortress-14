185188c854c16f7e44ffce5a03439e7dfa1f8f3c
poemota <142114334+poeMota@users.noreply.github.com>
2025-04-30 09:15:56 +1000

    map comp query

--- Content.Server/_RF/NPC/Queries/Queries/KeyComponentQuery.cs
+++ Content.Server/_RF/NPC/Queries/Queries/KeyComponentQuery.cs
@@ -1,30 +1,15 @@
 using Content.Server.NPC;
-using Robust.Server.GameObjects;
 
 namespace Content.Server._RF.NPC.Queries.Queries;
 
 
 public sealed partial class KeyComponentQuery : RfUtilityQuery
 {
-    private TransformSystem _transform = default!;
-    private EntityLookupSystem _lookup = default!;
-
-    private EntityQuery<TransformComponent> _xformQuery;
-
     /// <summary>
     /// Component to be filtered out
     /// </summary>
     [DataField(required: true)]
     public string Key = default!;
-
-    public override void Initialize(IEntityManager entManager)
-    {
-        base.Initialize(entManager);
-        _transform = entManager.System<TransformSystem>();
-        _lookup = entManager.System<EntityLookupSystem>();
-
-        _xformQuery = entManager.GetEntityQuery<TransformComponent>();
-    }
 
     public override HashSet<EntityUid> Query(NPCBlackboard blackboard)
     {
@@ -32,22 +17,17 @@ public sealed partial class KeyComponentQuery : RfUtilityQuery
             return new();
 
         var query = new HashSet<EntityUid>();
-        var entitySet = new HashSet<Entity<IComponent>>();
 
         var owner = blackboard.GetValue<EntityUid>(NPCBlackboard.Owner);
-        var vision = blackboard.GetValueOrDefault<float>(blackboard.GetVisionRadiusKey(EntityManager), EntityManager);
-        var mapPos = _transform.GetMapCoordinates(owner, xform: _xformQuery.GetComponent(owner));
         var comp = EntityManager.ComponentFactory.GetComponent(compId);
 
-        _lookup.GetEntitiesInRange(comp.GetType(), mapPos, vision * 10, entitySet);
-
-        foreach (var ent in entitySet)
+        var enumerator = EntityManager.AllEntityQueryEnumerator(comp.GetType());
+        while (enumerator.MoveNext(out var uid, out _))
         {
-            if (ent.Owner == owner)
+            if (uid == owner)
                 continue;
 
-            if (EntityManager.HasComponent(ent, comp.GetType()))
-                query.Add(ent);
+            query.Add(uid);
         }
 
         return query;
