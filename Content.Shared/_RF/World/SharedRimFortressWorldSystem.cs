2ec4c70f42d6b0582e37e0798468c7b428571113
poemota <142114334+poeMota@users.noreply.github.com>
2025-05-13 15:41:55 +1000

    add players settlements finding algorythm

--- Content.Shared/_RF/World/SharedRimFortressWorldSystem.cs
+++ Content.Shared/_RF/World/SharedRimFortressWorldSystem.cs
@@ -29,11 +29,13 @@ public abstract class SharedRimFortressWorldSystem : EntitySystem
     protected const byte ChunkSize = 8; // Copy of SharedBiomeSystem.ChunkSize
 
     protected EntityQuery<RimFortressPlayerComponent> PlayerQuery;
+    private EntityQuery<TransformComponent> _xformQuery;
 
     public override void Initialize()
     {
         base.Initialize();
         PlayerQuery = GetEntityQuery<RimFortressPlayerComponent>();
+        _xformQuery = GetEntityQuery<TransformComponent>();
     }
 
     #region Spawning
@@ -267,10 +269,41 @@ public abstract class SharedRimFortressWorldSystem : EntitySystem
     public List<EntityCoordinates> GetPlayerSettlements(Entity<RimFortressPlayerComponent?> player)
     {
         if (!Resolve(player, ref player.Comp)
+            || !_xformQuery.TryComp(player, out var playerXform)
             || player.Comp.Pops.Count == 0)
             return new();
 
-        return new() { Transform(player).Coordinates }; // TODO
+        // Collect a list of the coordinates of all the player's pops
+        var points = new List<Vector2>();
+        foreach (var pop in player.Comp.Pops)
+        {
+            if (!_xformQuery.TryComp(pop, out var xform))
+                continue;
+
+            points.Add(xform.Coordinates.Position);
+        }
+
+        var grid = playerXform.Coordinates.EntityId;
+        var coords = new List<EntityCoordinates>();
+
+        // Divide pop coordinates into clusters
+        var (clusters, _) = DbScan.Cluster(points, 100f, 1);
+
+        // Find the center of mass of all clusters of points
+        foreach (var cluster in clusters)
+        {
+            var massCenter = Vector2.Zero;
+
+            foreach (var point in cluster)
+            {
+                massCenter += point;
+            }
+
+            massCenter /= clusters.Count;
+            coords.Add(new EntityCoordinates(grid, massCenter));
+        }
+
+        return coords;
     }
 
     public List<EntityCoordinates> AllPlayersSettlements()
@@ -350,3 +383,83 @@ public sealed class PlayerAvailableForEvent : HandledEntityEventArgs
 {
     public Entity<RimFortressPlayerComponent> Player;
 }
+
+#region Helpers
+
+/// <summary>
+/// A class helper that extracts clusters and noise among an array of points using the DBSCAN algorithm
+/// </summary>
+public static class DbScan
+{
+    /// <summary>
+    /// Selects clusters and individual noise points among an array of points
+    /// </summary>
+    /// <param name="points">List of points from which to select a cluster</param>
+    /// <param name="radius">Maximum radius of one cluster</param>
+    /// <param name="minPts">Minimum required number of neighboring points to be considered as a cluster and not as noise</param>
+    /// <returns>List of point clusters and list of noisy points</returns>
+    public static (List<List<Vector2>> Clusters, List<Vector2> Noise) Cluster(
+        List<Vector2> points,
+        float radius,
+        int minPts)
+    {
+        List<List<Vector2>> clusters = new();
+        HashSet<Vector2> visited = new();
+        List<Vector2> noise = new();
+
+        foreach (var point in points)
+        {
+            if (!visited.Add(point))
+                continue;
+
+            var neighbors = GetNeighbors(points, point, radius);
+
+            if (neighbors.Count < minPts)
+            {
+                noise.Add(point);
+                continue;
+            }
+
+            List<Vector2> cluster = new();
+            ExpandCluster(points, point, neighbors, cluster, visited, radius, minPts);
+            clusters.Add(cluster);
+        }
+
+        return (clusters, noise);
+    }
+
+    private static void ExpandCluster(
+        List<Vector2> points,
+        Vector2 point,
+        List<Vector2> neighbors,
+        List<Vector2> cluster,
+        HashSet<Vector2> visited,
+        float radius,
+        int minPts)
+    {
+        cluster.Add(point);
+
+        for (var i = 0; i < neighbors.Count; i++)
+        {
+            var neighbor = neighbors[i];
+
+            if (visited.Add(neighbor))
+            {
+                var newNeighbors = GetNeighbors(points, neighbor, radius);
+
+                if (newNeighbors.Count >= minPts)
+                    neighbors.AddRange(newNeighbors);
+            }
+
+            if (!cluster.Contains(neighbor))
+                cluster.Add(neighbor);
+        }
+    }
+
+    private static List<Vector2> GetNeighbors(List<Vector2> points, Vector2 point, float radius)
+    {
+        return points.Where(p => Vector2.Distance(p, point) <= radius).ToList();
+    }
+}
+
+#endregion
