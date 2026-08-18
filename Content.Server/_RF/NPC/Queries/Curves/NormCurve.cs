4ed97b0785ed984e7a2c85fedaac5d39a7483c34
Mota <belochuc@gmail.com>
2026-08-18 14:22:17 +0300

    remove legacy HTN stuff

--- Content.Server/_RF/NPC/Queries/Curves/NormCurve.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Server._RF.NPC.Queries.Curves;
-
-/// <summary>
-/// Normalizes the input value
-/// </summary>
-public sealed partial class NormCurve : RfUtilityCurve
-{
-    public override float Curve(float value) => 1 - 1 / value;
-}
