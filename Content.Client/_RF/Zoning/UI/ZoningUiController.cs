0c689327177fdd9b4ead39cfd2dac32f8e908d7d
Mota <belochuc@gmail.com>
2026-09-14 18:47:28 +0300

    tooltip fixes

--- Content.Client/_RF/Zoning/UI/ZoningUiController.cs
+++ Content.Client/_RF/Zoning/UI/ZoningUiController.cs
@@ -348,7 +348,8 @@ public sealed partial class ZoningUiController :
 
         if (map == MapCoordinates.Nullspace)
         {
-            //ClosePopup(); TODO
+            if (_tileConditions?.Popup.Pinned != true)
+                ClosePopup();
             return;
         }
 
